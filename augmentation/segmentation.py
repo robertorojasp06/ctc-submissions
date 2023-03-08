afd64c885f31cc8e7eef49a897713486de4cbeb8
Roberto Rojas <roberto.rojasp06@gmail.com>
2023-03-08 15:59:18 -0300

    Add new transforms for augmentation during training.
    - RandomBrightness
    - RandomSliceBrightness
    - RandomElasticDeformation (not integrated to OnlineAugmentation)

--- augmentation/segmentation.py
+++ augmentation/segmentation.py
@@ -15,19 +15,29 @@ PROBABILITIES = {
         'rotation': 1 / 3
     },
     'intensity': {
-        'gaussian_noise': 0.5,
-        'blurring': 0.5
+        'gaussian_noise': 0.25,
+        'blurring': 0.25,
+        'brightness': 0.25,
+        'slice_brightness': 0.25
     }
 }
 
 
 class OnlineAugmentation:
     def __init__(self, probabilities=PROBABILITIES, max_noise_mean=0,
-                 max_noise_std=0.05, max_blur_std=2):
+                 max_noise_std=0.05, max_blur_std=2, min_brightness=0.7,
+                 max_brightness=1.25, min_slice_brightness=0.5,
+                 max_slice_brightness=0.8):
         self.probabilities = probabilities
         self.max_noise_mean = max_noise_mean
         self.max_noise_std = max_noise_std
         self.max_blur_std = max_blur_std
+        self.min_brightness = min_brightness
+        self.max_brightness = max_brightness
+        self.min_slice_brightness = min_slice_brightness
+        self.max_slice_brightness = max_slice_brightness
+        self.slice_brightness_min_slices = 1
+        self.slice_brightness_max_slices = 2
         self.axes_options = [0, 1, 2, (0, 1), (0, 2), (1, 2), (0, 1, 2)]
         self.angle_options = [90, 180, 270]
 
@@ -53,6 +63,18 @@ class OnlineAugmentation:
                 self.max_blur_std,
                 probability=1.0
             )
+            brightness = transforms.RandomBrightness(
+                self.min_brightness,
+                self.max_brightness,
+                probability=1.0
+            )
+            slice_brightness = transforms.RandomSliceBrightness(
+                self.min_slice_brightness,
+                self.max_slice_brightness,
+                self.slice_brightness_min_slices,
+                self.slice_brightness_max_slices,
+                probability=1.0
+            )
             spatial_transform = RandomChoice(
                 [
                     identity,
@@ -68,11 +90,15 @@ class OnlineAugmentation:
             intensity_transform = RandomChoice(
                 [
                     noise,
-                    blurring
+                    blurring,
+                    brightness,
+                    slice_brightness
                 ],
                 [
                     self.probabilities['intensity']['gaussian_noise'],
-                    self.probabilities['intensity']['blurring']
+                    self.probabilities['intensity']['blurring'],
+                    self.probabilities['intensity']['brightness'],
+                    self.probabilities['intensity']['slice_brightness']
                 ]
             )
             sample = intensity_transform(spatial_transform(sample))
